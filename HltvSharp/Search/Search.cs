3b429505b1182b4380968f7e1dc37cccde00f6b1
Arttu Kuikka <arttu@arttukuikka.fi>
2022-04-07 18:33:02 +0300

    added AllSearch

--- HltvSharp/Search/Search.cs
+++ HltvSharp/Search/Search.cs
@@ -134,6 +134,129 @@ namespace HltvSharp
             return EventList;
         }
 
+        public async Task<AllSearchItem> All(string SearchQuery)
+        {
+            var item = new AllSearchItem();
+
+            var Results = await GetSearchResults(SearchQuery);
+
+
+            //teams
+            var Teamlist = new List<TeamSearchItem>();
+
+            var TeamArray = Results["teams"];
+            if (TeamArray == null) { return null; }
+
+            foreach (var Team in TeamArray)
+            {
+                var TeamItem = new TeamSearchItem();
+
+                TeamItem.Name = (string)Team["name"];
+                TeamItem.Id = (int)Team["id"];
+                TeamItem.flagUrl = (string)Team["flagUrl"];
+                TeamItem.teamLogoDarkUrl = (string)Team["teamLogoNight"];
+                TeamItem.teamLogoLightUrl = (string)Team["teamLogoDay"];
+                TeamItem.webLocation = (string)Team["location"];
+
+                var TeamPlayerList = new List<TeamSearchPlayerItem>();
+
+                var TeamPlayerArray = Team["players"] as JArray;
+
+                foreach (var Player in TeamPlayerArray)
+                {
+                    var PlayerItem = new TeamSearchPlayerItem();
+                    PlayerItem.firstName = (string)Player["firstName"];
+                    PlayerItem.lastName = (string)Player["lastName"];
+                    PlayerItem.nickName = (string)Player["nickName"];
+                    PlayerItem.flagUrl = (string)Player["flagUrl"];
+                    PlayerItem.webLocation = (string)Player["location"];
+
+                    TeamPlayerList.Add(PlayerItem);
+                }
+
+                TeamItem.PlayerList = TeamPlayerList;
+
+                Teamlist.Add(TeamItem);
+            }
+
+            item.Teams = Teamlist;
+
+
+            //players
+            var PlayerList = new List<PlayerSearchItem>();
+
+            var PlayerArray = Results["players"];
+            if (PlayerArray == null) { return null; }
+
+            foreach (var Player in PlayerArray)
+            {
+                var PlayerItem = new PlayerSearchItem();
+
+                PlayerItem.firstName = (string)Player["firstName"];
+                PlayerItem.lastName = (string)Player["firstName"];
+                PlayerItem.nickName = (string)Player["nickName"];
+                PlayerItem.flagUrl = (string)Player["flagUrl"];
+                PlayerItem.webLocation = (string)Player["location"];
+                PlayerItem.id = (int)Player["id"];
+                PlayerItem.pictureUrl = (string)Player["pictureUrl"];
+
+                var PlayerTeamItem = Player["team"];
+
+                var PlayerTeam = new PlayerTeamItem();
+
+                if (PlayerTeamItem != null)
+                {
+                    PlayerTeam.name = (string)PlayerTeamItem["name"];
+                    PlayerTeam.teamLogoDarkUrl = (string)PlayerTeamItem["teamLogoNight"];
+                    PlayerTeam.teamLogoLightUrl = (string)PlayerTeamItem["teamLogoDay"];
+                    PlayerTeam.webLocation = (string)PlayerTeamItem["location"];
+                }
+                else
+                {
+                    PlayerTeam = null;
+                }
+
+                PlayerItem.team = PlayerTeam;
+
+                PlayerList.Add(PlayerItem);
+            }
+
+            item.Players = PlayerList;
+
+
+            //events
+            var EventList = new List<EventsSearchItem>();
+
+            var EventArray = Results["events"];
+            if (EventArray == null) { return null; }
+
+            foreach (var Event in EventArray)
+            {
+                var EventItem = new EventsSearchItem();
+
+
+
+
+                EventItem.id = (int)Event["id"];
+                EventItem.name = (string)Event["name"];
+                EventItem.flagUrl = (string)Event["flagUrl"];
+                EventItem.flagUrl = (string)Event["flagUrl"];
+                EventItem.eventLogoUrl = (string)Event["eventLogo"];
+                EventItem.webLocation = (string)Event["location"];
+                EventItem.physicalLocation = (string)Event["physicalLocation"];
+                EventItem.prizePool = (string)Event["prizePool"];
+                EventItem.eventType = (string)Event["eventType"];
+
+                EventList.Add(EventItem);
+            }
+
+            item.Events = EventList;
+
+
+
+            return item;
+        }
+
         public async Task<JToken> RawJson(string SearchQuery)
         {
             var Results = await GetSearchResults(SearchQuery);
